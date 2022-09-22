0d315db1e8ef569886aadd2e9a16cda4690cde1b
antishsh <antenehshimelis2019@gmail.com>
2022-09-22 05:22:25 -0700

    hi

--- 120-binary_tree_is_avl.c
+++ 120-binary_tree_is_avl.c
@@ -9,10 +9,9 @@
  */
 int binary_tree_is_avl(const binary_tree_t *tree)
 {
-    if (!tree)
-        return (0);
-
-    return (btia_helper(tree, INT_MIN, INT_MAX));
+	if (!tree)
+		return (0);
+	return (btia_helper(tree, INT_MIN, INT_MAX));
 }
 
 /**
@@ -26,21 +25,20 @@ int binary_tree_is_avl(const binary_tree_t *tree)
  */
 int btia_helper(const binary_tree_t *tree, int min, int max)
 {
-    int path_l, path_r;
+	int path_l, path_r;
 
-    if (!tree)
-        return (1);
-    if (tree->n < min || tree->n > max)
-        return (0);
+	if (!tree)
+		return (1);
+	if (tree->n < min || tree->n > max)
+		return (0);
+	path_l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	path_r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 
-    path_l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-    path_r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+	if (abs(path_l - path_r) > 1)
+		return (0);
 
-    if (abs(path_l - path_r) > 1)
-        return (0);
-
-    return (btia_helper(tree->left, min, tree->n - 1) &&
-            btia_helper(tree->right, tree->n + 1, max));
+	return (btia_helper(tree->left, min, tree->n - 1) &&
+			btia_helper(tree->right, tree->n + 1, max));
     /* This is part of the BST check logic */
 }
 
@@ -53,13 +51,12 @@ int btia_helper(const binary_tree_t *tree, int min, int max)
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    size_t height_l = 0;
-    size_t height_r = 0;
+	size_t height_l = 0;
+	size_t height_r = 0;
 
-    if (!tree)
-        return (0);
-
-    height_l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-    height_r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
-    return (height_l > height_r ? height_l : height_r);
+	if (!tree)
+		return (0);
+	height_l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	height_r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+	return (height_l > height_r ? height_l : height_r);
 }
--- 121-avl_insert.c
+++ 121-avl_insert.c
@@ -9,38 +9,38 @@
  */
 bst_t *bst_insert(bst_t **tree, int value)
 {
-    bst_t *tmp = *tree;
-    bst_t *second = NULL;
-    bst_t *new = binary_tree_node(NULL, value);
+	bst_t *tmp = *tree;
+	bst_t *second = NULL;
+	bst_t *new = binary_tree_node(NULL, value);
 
-    if (*tree == NULL)
-        *tree = new;
+	if (*tree == NULL)
+		*tree = new;
 
-    while (tmp)
-    {
-        second = tmp;
-        if (value < tmp->n)
-            tmp = tmp->left;
-        else if (value > tmp->n)
-            tmp = tmp->right;
-        else if (value == tmp->n)
-            return (NULL);
-    }
+	while (tmp)
+	{
+		second = tmp;
 
-    if (second == NULL)
-        second = new;
-    else if (value < second->n)
-    {
-        second->left = new;
-        new->parent = second;
-    }
-    else
-    {
-        second->right = new;
-        new->parent = second;
-    }
+		if (value < tmp->n)
+			tmp = tmp->left;
+		else if (value > tmp->n)
+			tmp = tmp->right;
+		else if (value == tmp->n)
+			return (NULL);
+	}
 
-    return (new);
+	if (second == NULL)
+		second = new;
+	else if (value < second->n)
+	{
+		second->left = new;
+		new->parent = second;
+	}
+	else
+	{
+		second->right = new;
+		new->parent = second;
+	}
+	return (new);
 }
 
 /**
@@ -53,25 +53,25 @@ bst_t *bst_insert(bst_t **tree, int value)
  */
 avl_t *avl_insert(avl_t **tree, int value)
 {
-    int balance = 0;
-    avl_t *node = bst_insert(tree, value);
+	int balance = 0;
+	avl_t *node = bst_insert(tree, value);
 
-    balance = binary_tree_balance(*tree);
+	balance = binary_tree_balance(*tree);
 
-    if (balance > 1 && value < node->left->n)
-        return (binary_tree_rotate_right(node));
-    if (balance < -1 && value > node->right->n)
-        return (binary_tree_rotate_left(node));
-    if (balance > 1 && value > node->left->n)
-    {
-        node->left = binary_tree_rotate_left(node->left);
-        return (binary_tree_rotate_right(node));
-    }
-    if (balance < -1 && value < node->right->n)
-    {
-        node->right = binary_tree_rotate_right(node->right);
-        return (binary_tree_rotate_left(node));
-    }
+	if (balance > 1 && value < node->left->n)
+		return (binary_tree_rotate_right(node));
+	if (balance < -1 && value > node->right->n)
+		return (binary_tree_rotate_left(node));
+	if (balance > 1 && value > node->left->n)
+	{
+		node->left = binary_tree_rotate_left(node->left);
+		return (binary_tree_rotate_right(node));
+	}
 
-    return (node);
+	if (balance < -1 && value < node->right->n)
+	{
+		node->right = binary_tree_rotate_right(node->right);
+		return (binary_tree_rotate_left(node));
+	}
+	return (node);
 }
