659d3c68f42e1e36933c96f43e8eb4f8dc961705
antishsh <antenehshimelis2019@gmail.com>
2022-09-22 05:31:37 -0700

    correction

--- 130-binary_tree_is_heap.c
+++ 130-binary_tree_is_heap.c
@@ -10,9 +10,9 @@
  */
 int binary_tree_is_heap(const binary_tree_t *tree)
 {
-    if (!tree)
-        return (0);
-    return (btih_helper(tree));
+	if (!tree)
+		return (0);
+	return (btih_helper(tree));
 }
 
 /**
@@ -25,21 +25,18 @@ int binary_tree_is_heap(const binary_tree_t *tree)
  */
 int btih_helper(const binary_tree_t *tree)
 {
-    if (!tree)
-        return (1);
-
-    if (!binary_tree_is_complete(tree))
-        return (0);
-
-    if (tree->left)
-        if (tree->left->n > tree->n)
-            return (0);
-    if (tree->right)
-        if (tree->right->n > tree->n)
-            return (0);
-
-    return (btih_helper(tree->left) &&
-            btih_helper(tree->right));
+	if (!tree)
+		return (1);
+	if (!binary_tree_is_complete(tree))
+		return (0);
+	if (tree->left)
+		if (tree->left->n > tree->n)
+			return (0);
+	if (tree->right)
+		if (tree->right->n > tree->n)
+			return (0);
+	return (btih_helper(tree->left) &&
+			btih_helper(tree->right));
 }
 
 /**
@@ -52,13 +49,12 @@ int btih_helper(const binary_tree_t *tree)
  */
 int binary_tree_is_complete(const binary_tree_t *tree)
 {
-    size_t size;
+	size_t size;
 
-    if (!tree)
-        return (0);
-    size = binary_tree_size(tree);
-
-    return (btic_helper(tree, 0, size));
+	if (!tree)
+		return (0);
+	size = binary_tree_size(tree);
+	return (btic_helper(tree, 0, size));
 }
 
 /**
@@ -73,14 +69,12 @@ int binary_tree_is_complete(const binary_tree_t *tree)
  */
 int btic_helper(const binary_tree_t *tree, size_t index, size_t size)
 {
-    if (!tree)
-        return (1);
-
-    if (index >= size)
-        return (0);
-
-    return (btic_helper(tree->left, 2 * index + 1, size) &&
-            btic_helper(tree->right, 2 * index + 2, size));
+	if (!tree)
+		return (1);
+	if (index >= size)
+		return (0);
+	return (btic_helper(tree->left, 2 * index + 1, size) &&
+			btic_helper(tree->right, 2 * index + 2, size));
 }
 
 /**
@@ -92,9 +86,8 @@ int btic_helper(const binary_tree_t *tree, size_t index, size_t size)
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-    if (!tree)
-        return (0);
-
-    return (binary_tree_size(tree->left) +
-            binary_tree_size(tree->right) + 1);
+	if (!tree)
+		return (0);
+	return (binary_tree_size(tree->left) +
+			binary_tree_size(tree->right) + 1);
 }
