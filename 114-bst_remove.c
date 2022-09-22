fa61737b20619d520ae2c67cf346c5479908db11
antishsh <antenehshimelis2019@gmail.com>
2022-09-22 05:03:34 -0700

    add

--- 114-bst_remove.c
+++ 114-bst_remove.c
@@ -9,34 +9,33 @@
  */
 bst_t *bst_remove(bst_t *root, int value)
 {
-    bst_t *tmp = NULL;
+	bst_t *tmp = NULL;
 
-    if (!root)
-        return (NULL);
-
-    if (value < root->n)
-        root->left = bst_remove(root->left, value);
-    else if (value > root->n)
-        root->right = bst_remove(root->right, value);
-    else
-    {
-        if (!root->left)
-        {
-            tmp = root->right;
-            free(root);
-            return (tmp);
-        }
-        else if (!root->right)
-        {
-            tmp = root->left;
-            free(root);
-            return (tmp);
-        }
-        tmp = bst_min_val(root->right);
-        root->n = tmp->n;
-        root->right = bst_remove(root->right, tmp->n);
-    }
-    return (root);
+	if (!root)
+		return (NULL);
+	if (value < root->n)
+		root->left = bst_remove(root->left, value);
+	else if (value > root->n)
+		root->right = bst_remove(root->right, value);
+	else
+	{
+		if (!root->left)
+		{
+			tmp = root->right;
+			free(root);
+			return (tmp);
+		}
+		else if (!root->right)
+		{
+			tmp = root->left;
+			free(root);
+			return (tmp);
+		}
+		tmp = bst_min_val(root->right);
+		root->n = tmp->n;
+		root->right = bst_remove(root->right, tmp->n);
+	}
+	return (root);
 }
 
 /**
@@ -46,10 +45,9 @@ bst_t *bst_remove(bst_t *root, int value)
  */
 bst_t *bst_min_val(bst_t *root)
 {
-    bst_t *min = root;
+	bst_t *min = root;
 
-    while (min->left)
-        min = min->left;
-
-    return (min);
+	while (min->left)
+		min = min->left;
+	return (min);
 }
