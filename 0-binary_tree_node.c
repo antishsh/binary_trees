fc54e2b1593186370c7f89fde036e26691de00e0
antishsh <antenehshimelis2019@gmail.com>
2022-09-21 23:54:11 -0700

    c

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -3,16 +3,11 @@
 
 
 /**
- *
- *  *binary_tree_node - creates a binary tree node
- *
- *   *@parent: pointer to the parent node of the node to create
- *
- *    *@value: value to put in the new node
- *
- *     *Return: The returning the node
- *
- *      */
+ * binary_tree_node - creates a binary tree node
+ * @parent: pointer to the parent node of the node to create
+ * @value: value to put in the new node
+ * Return: The returning the node
+ */
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 
--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -3,16 +3,11 @@
 
 
 /**
- *
- *  *binary_tree_insert_left - inserts a node as the left-child of another node
- *
- *   *@parent: pointer to the parent node of the node to create
- *
- *    *@value: value to put in the new node
- *
- *     *Return: The returning the node
- *
- *      */
+ * binary_tree_insert_left - inserts a node as the left-child of another node
+ * @parent: pointer to the parent node of the node to create
+ * @value: value to put in the new node      
+ * Return: The returning the node
+ */
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 
