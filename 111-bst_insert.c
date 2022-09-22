6c142a36149b2a1672ab45283e7979d3539fd215
antishsh <antenehshimelis2019@gmail.com>
2022-09-22 04:30:40 -0700

    corr

--- 111-bst_insert.c
+++ 111-bst_insert.c
@@ -9,40 +9,45 @@
  */
 bst_t *bst_insert(bst_t **tree, int value)
 {
-    bst_t *tmp = NULL;
-    bst_t *second = NULL;
-    bst_t *new = NULL;
+	bst_t *tmp = NULL;
+	bst_t *second = NULL;
+	bst_t *new = NULL;
 
-    if (!tree)
-	    return (NULL);
-    if (*tree == NULL)
-        return (*tree = binary_tree_node(NULL, value));
+	if (!tree)
+		return (NULL);
 
-    tmp = *tree;
-    while (tmp)
-    {
-        second = tmp;
-        if (value < tmp->n)
-            tmp = tmp->left;
-        else if (value > tmp->n)
-            tmp = tmp->right;
-        else if (value == tmp->n)
-            return (NULL);
-    }
+	if (*tree == NULL)
+		return (*tree = binary_tree_node(NULL, value));
 
-    new = binary_tree_node(NULL, value);
-    if (second == NULL)
-        second = new;
-    else if (value < second->n)
-    {
-        second->left = new;
-        new->parent = second;
-    }
-    else
-    {
-        second->right = new;
-        new->parent = second;
-    }
+	tmp = *tree;
 
-    return (new);
+	while (tmp)
+	{
+		second = tmp;
+
+		if (value < tmp->n)
+			tmp = tmp->left;
+
+		else if (value > tmp->n)
+			tmp = tmp->right;
+		else if (value == tmp->n)
+			return (NULL);
+	}
+
+	new = binary_tree_node(NULL, value);
+
+	if (second == NULL)
+		second = new;
+
+	else if (value < second->n)
+	{
+		second->left = new;
+		new->parent = second;
+	}
+	else
+	{
+		second->right = new;
+		new->parent = second;
+	}
+	return (new);
 }
