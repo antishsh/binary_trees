9a0a80dbb6ec86faf7037e0c7f9aee981e686119
antish <AntenehShi@ethiopianairlines.com>
2022-09-22 21:53:01 +0300

    cf

--- 121-avl_insert.c
+++ 121-avl_insert.c
@@ -5,24 +5,7 @@ int balance(const binary_tree_t *tree);
 avl_t *avl_insert_recursive(avl_t **tree, avl_t *parent,
 		avl_t **new, int value);
 avl_t *avl_insert(avl_t **tree, int value);
-/**
- * height - Measures the height of a binary tree.
- * @tree: A pointer to the root node of the tree to measure the height.
- *
- * Return: If tree is NULL, your function must return 0, else return height.
- */
-size_t height(const binary_tree_t *tree)
-{
-	if (tree != NULL)
-	{
-		size_t l = 0, r = 0;
 
-		l = tree->left ? 1 + binary_tree_height(tree->left) : 1;
-		r = tree->right ? 1 + binary_tree_height(tree->right) : 1;
-		return ((l > r) ? l : r);
-	}
-	return (0);
-}
 /**
  * balance - Measures the balance factor of a binary tree.
  * @tree: A pointer to the root node of the tree to measure the balance factor.
@@ -31,8 +14,8 @@ size_t height(const binary_tree_t *tree)
  */
 int balance(const binary_tree_t *tree)
 {
-	return (tree != NULL ? custom_binary_tree_height(tree->left) -
-			custom_binary_tree_height(tree->right) : 0);
+	return (tree != NULL ? custom_binary_tree_height(tree->left) - 
+	custom_binary_tree_height(tree->right) : 0);
 }
 
 /**
