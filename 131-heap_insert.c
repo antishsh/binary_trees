b96da8acf66654794fa1f6e80facdc12a8cee993
antishsh <antenehshimelis2019@gmail.com>
2022-09-22 06:46:31 -0700

    add

--- 131-heap_insert.c
+++ 131-heap_insert.c
@@ -61,8 +61,7 @@ heap_t *heap_insert(heap_t **root, int value)
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-    if (!tree)
-        return (0);
-
-    return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
+	if (!tree)
+		return (0);
+	return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
 }
